e0a661a46a569ea5ae263c2e81f0248003470faa
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-06-29 20:32:15 -0300

    remove flat from v-app-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,7 +35,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar clipped-left fixed app flat>
+    <v-app-bar clipped-left fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
