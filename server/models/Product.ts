84ffc845a448ce946e3a18b01398f5b52083e29f
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-07-05 21:21:02 -0300

    add slug in product

--- server/models/Product.ts
+++ server/models/Product.ts
@@ -1,12 +1,15 @@
 import { Field, ID, ObjectType } from 'type-graphql'
 import {
   BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   DeleteDateColumn,
   Entity,
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm'
+import slugify from 'slugify'
 import { Category } from './Category'
 
 @Entity()
@@ -31,6 +34,19 @@ export class Product extends BaseEntity {
   @Field()
   @Column({ type: 'decimal' })
   public price!: number
+
+  @Field()
+  @Column({ default: '' })
+  public slug!: string
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  update() {
+    this.slug = slugify(this.name, {
+      replacement: '-',
+      lower: true,
+    })
+  }
 
   @Field()
   @DeleteDateColumn()
