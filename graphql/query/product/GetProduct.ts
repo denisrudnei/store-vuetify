9f0e4d415e621275dd79a6076c04395ec926cb48
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-07-05 21:04:19 -0300

    get product info in product page

---
+++ graphql/query/product/GetProduct.ts
@@ -0,0 +1,12 @@
+import ggl from 'graphql-tag'
+
+export const GetProduct = ggl`
+query GetProduct($id: ID!) {
+  GetProduct (id: $id) {
+    id
+    name
+    description
+    price
+  }
+}
+`
--- pages/categories/_category/product/_id.vue
+++ pages/categories/_category/product/_id.vue
@@ -46,54 +46,7 @@
       <v-row>
         <v-col cols="12">
           <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel quam
-            elementum pulvinar etiam non quam lacus suspendisse. Morbi enim nunc
-            faucibus a pellentesque sit amet porttitor eget. Nunc mi ipsum
-            faucibus vitae aliquet nec ullamcorper sit. Vitae justo eget magna
-            fermentum iaculis. In pellentesque massa placerat duis. Elit ut
-            aliquam purus sit amet luctus venenatis. Nibh ipsum consequat nisl
-            vel pretium lectus quam id leo. Mattis rhoncus urna neque viverra
-            justo nec ultrices dui sapien. Mi tempus imperdiet nulla malesuada
-            pellentesque elit eget gravida cum. Nibh mauris cursus mattis
-            molestie. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus
-            vitae. Ac turpis egestas sed tempus urna et pharetra. Feugiat
-            scelerisque varius morbi enim nunc. Magna sit amet purus gravida
-            quis blandit turpis. Et netus et malesuada fames. Consectetur a erat
-            nam at. Nulla aliquet enim tortor at auctor urna nunc id cursus.
-            Ultricies mi quis hendrerit dolor magna eget est. Maecenas accumsan
-            lacus vel facilisis volutpat. Ut tortor pretium viverra suspendisse
-            potenti nullam ac. Morbi tristique senectus et netus. Neque
-            convallis a cras semper auctor. Sit amet justo donec enim diam.
-            Volutpat consequat mauris nunc congue nisi vitae suscipit tellus
-            mauris. Consectetur libero id faucibus nisl tincidunt eget. Ut sem
-            viverra aliquet eget sit. Viverra adipiscing at in tellus integer.
-            Erat nam at lectus urna. Ipsum dolor sit amet consectetur adipiscing
-            elit. Proin libero nunc consequat interdum varius. Pellentesque
-            massa placerat duis ultricies. Etiam non quam lacus suspendisse.
-            Fringilla ut morbi tincidunt augue interdum velit euismod in
-            pellentesque. Ridiculus mus mauris vitae ultricies. Nunc scelerisque
-            viverra mauris in aliquam sem. Interdum consectetur libero id
-            faucibus nisl tincidunt eget nullam. Turpis in eu mi bibendum neque
-            egestas congue. Pellentesque nec nam aliquam sem et tortor. Aliquet
-            nibh praesent tristique magna sit. Blandit cursus risus at ultrices
-            mi tempus imperdiet nulla malesuada. Turpis massa tincidunt dui ut
-            ornare lectus sit amet. Viverra justo nec ultrices dui sapien.
-            Tristique senectus et netus et malesuada fames ac turpis egestas.
-            Nunc eget lorem dolor sed. Est velit egestas dui id. Aliquet
-            porttitor lacus luctus accumsan tortor. A scelerisque purus semper
-            eget duis at tellus at. Sagittis nisl rhoncus mattis rhoncus. Semper
-            risus in hendrerit gravida rutrum quisque non tellus. Eu tincidunt
-            tortor aliquam nulla. Ullamcorper dignissim cras tincidunt lobortis
-            feugiat. Ac auctor augue mauris augue neque gravida in fermentum et.
-            Ultrices in iaculis nunc sed augue. Hendrerit gravida rutrum quisque
-            non tellus orci ac auctor. Aliquam vestibulum morbi blandit cursus
-            risus at ultrices mi. Ultricies lacus sed turpis tincidunt id
-            aliquet risus feugiat in. Rhoncus est pellentesque elit ullamcorper
-            dignissim. Eu sem integer vitae justo eget magna. Eget mi proin sed
-            libero enim sed. Ultrices vitae auctor eu augue ut. Morbi tristique
-            senectus et netus. Blandit volutpat maecenas volutpat blandit
-            aliquam etiam erat velit scelerisque.
+            {{ product.description }}
           </span>
         </v-col>
       </v-row>
@@ -102,19 +55,12 @@
 </template>
 
 <script>
-import faker from 'faker'
 import { TheMask } from 'vue-the-mask'
+import { GetProduct } from '../../../../graphql/query/product/GetProduct'
 import Rating from '~/components/rating.vue'
 export default {
   directives: {
     TheMask,
-  },
-  async asyncData({ $axios, route }) {
-    const id = route.params.id
-    const { data } = await $axios.get(`/product/${id}`)
-    return {
-      product: data,
-    }
   },
   data() {
     return {
@@ -178,8 +124,16 @@ export default {
     },
   },
   created() {
-    this.product.price = faker.commerce.price()
-    this.product.name = faker.commerce.productName()
+    this.$apollo
+      .query({
+        query: GetProduct,
+        variables: {
+          id: this.$route.params.id,
+        },
+      })
+      .then((response) => {
+        this.product = response.data.GetProduct
+      })
   },
   methods: {
     addToCart(product) {
