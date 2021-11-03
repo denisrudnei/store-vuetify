32434d85a793ccd98198b27ea336be65a89bef1a
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-11-03 20:43:16 -0300

    notify for new purchase on POS

--- server/resolvers/POSPurchaseResolver.ts
+++ server/resolvers/POSPurchaseResolver.ts
@@ -6,8 +6,10 @@ import {
   Mutation,
   Query,
   Resolver,
+  PubSub,
 } from 'type-graphql'
 
+import { PubSubEngine } from 'graphql-subscriptions'
 import { Role } from '../enums/Role'
 import { POS } from '../models/POS'
 import { Purchase } from '../models/Purchase'
@@ -16,6 +18,8 @@ import { CustomExpressContext } from '../types/CustomExpressContext'
 import { ProductForPurchaseInput } from '../inputs/ProductForPurchaseInput'
 import { PaymentInput } from '../inputs/PaymentInput'
 import { User } from '../models/User'
+import { PurchaseEvents } from '../enums/PurchaseEvents'
+import { SummaryEvents } from '../enums/SummaryEvents'
 
 @Resolver(() => Purchase)
 export class POSPurchaseResolver {
@@ -27,15 +31,25 @@ export class POSPurchaseResolver {
 
   @Mutation(() => Purchase)
   @Authorized(Role.OPERATOR)
-  public PurchaseFromPOS(
+  public async PurchaseFromPOS(
     @Arg('user', () => ID, { nullable: true }) user: User['id'],
     @Arg('products', () => [ProductForPurchaseInput])
     products: ProductForPurchaseInput[],
     @Arg('payment', () => [PaymentInput])
     payment: PaymentInput[],
+    @PubSub() pubSub: PubSubEngine,
     @Ctx() { req }: CustomExpressContext
   ) {
     const id = req.session.authUser!.id
-    return POSPurchaseService.createPurchase(id, user, products, payment)
+
+    const purchase = await POSPurchaseService.createPurchase(
+      id,
+      user,
+      products,
+      payment
+    )
+    pubSub.publish(PurchaseEvents.NEW_PURCHASE, purchase)
+    pubSub.publish(SummaryEvents.UPDATE_SUMMARY, true)
+    return purchase
   }
 }
