ff00800fc9108273e423ccc8519161fcde121319
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-12-12 18:00:23 -0300

    set user to phone

--- server/resolvers/PhoneResolver.ts
+++ server/resolvers/PhoneResolver.ts
@@ -5,6 +5,7 @@ import { CreatePhoneInput } from '../inputs/CreatePhoneInput'
 import { Phone } from '../models/Phone'
 import { PhoneService } from '../services/PhoneService'
 import { CustomExpressContext } from '../types/CustomExpressContext'
+import { User } from '../models/User'
 
 @Resolver()
 export class PhoneResolver {
@@ -12,9 +13,16 @@ export class PhoneResolver {
   @Authorized(Role.USER, Role.OPERATOR)
   public CreatePhone(
     @Arg('phone', () => CreatePhoneInput) phone: CreatePhoneInput,
-    @Ctx() { req }: CustomExpressContext
+    @Ctx() { req }: CustomExpressContext,
+    @Arg('user', () => ID, { nullable: true }) userId?: User['id']
   ) {
-    const id = req.session.authUser!.id
+    let id = req.session.authUser!.id
+    if (
+      userId &&
+      [Role.ADMIN, Role.OPERATOR].includes(req.session.authUser!.role)
+    ) {
+      id = userId
+    }
     return PhoneService.create(phone, id)
   }
 
@@ -22,9 +30,17 @@ export class PhoneResolver {
   @Authorized(Role.USER, Role.OPERATOR)
   public RemovePhone(
     @Arg('id', () => ID) phone: Phone['id'],
-    @Ctx() { req }: CustomExpressContext
+    @Ctx() { req }: CustomExpressContext,
+    @Arg('user', () => ID, { nullable: true }) userId?: User['id']
   ) {
-    const id = req.session.authUser!.id
+    let id = req.session.authUser!.id
+    if (
+      userId &&
+      [Role.ADMIN, Role.OPERATOR].includes(req.session.authUser!.role)
+    ) {
+      id = userId
+    }
+
     return PhoneService.remove(phone, id)
   }
 }
