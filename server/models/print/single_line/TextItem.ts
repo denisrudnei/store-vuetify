fbf5f9da206f44566b520cf093031e8f59ac099d
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2021-12-19 12:30:48 -0300

    add text field in textItem

--- server/models/print/single_line/TextItem.ts
+++ server/models/print/single_line/TextItem.ts
@@ -1,9 +1,13 @@
-import { ObjectType } from 'type-graphql'
-import { Entity } from 'typeorm'
+import { Field, ObjectType } from 'type-graphql'
+import { Column, Entity } from 'typeorm'
 
 import { PrintLayoutItem } from '../PrintLayoutItem'
 import { SingleLineItemLayout } from './SingleLineItemLayout'
 
 @Entity()
 @ObjectType({ implements: [PrintLayoutItem, SingleLineItemLayout] })
-export class TextItem extends SingleLineItemLayout {}
+export class TextItem extends SingleLineItemLayout {
+  @Field()
+  @Column()
+  public text: string = ''
+}
